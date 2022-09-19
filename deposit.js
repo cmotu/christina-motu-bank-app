48a892c58fa9e1ba226967796541e03a32676b2c
cmotu <84061829+cmotu@users.noreply.github.com>
2022-09-19 15:25:30 -0700

    Add files via upload

---
+++ deposit.js
@@ -0,0 +1,11 @@
+export default function Deposit() {
+  return (
+    <Card
+      txtcolor="black"
+      header="BadBank Deposit"
+      title="Welcome to the bank"
+      text="Deposit your money below."
+      body={""}
+    />
+  );
+}
---
+++ withdraw.js
@@ -0,0 +1,11 @@
+function Withdraw() {
+  return (
+    <Card
+      txtcolor="black"
+      header="BadBank Withdrawal"
+      title="Welcome to the bank"
+      text="Withdraw your money below."
+      body={""}
+    />
+  );
+}
